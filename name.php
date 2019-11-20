6330c29e7d6784d3c46f62c25411bc43c17eaca3
FIRST_Imad LAST_Kajdouh <2058092@talnet.nl>
2019-11-20 12:06:13 +0100

    Uitwerking van de exercise

---
+++ name.php
@@ -0,0 +1,14 @@
+<?php
+echo "wat is jou voornaam?\n";
+ $a = readline();
+ echo $a, "\n";
+ echo "wat is jou achternaam?\n";
+ $b = readline ();
+ echo $b, "\n";
+ 
+ echo "Jouw naam is:";
+ echo $a ." ". $b;
+
+
+
+?>
